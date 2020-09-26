3ceb0d906476eff5bb52e019925cabd9f51445ad
FomichevEduard <Fomicheveduard1@yandex.ru>
2020-09-26 22:11:45 +0300

    4.5. Отладка программы в IDEA [#366093]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -11,7 +11,7 @@ public class SqArea {
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
+        double result1 = SqArea.square(6, 12);
         System.out.println(" p = 6, k = 2, s = 1, real = " + result1);
 
     }
